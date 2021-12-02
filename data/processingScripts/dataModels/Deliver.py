689f1b2a9fc0e1a4d8eb454cd0fdf39c40a53802
rbosko <rbosko@ittouch.pl>
2021-12-02 18:16:11 +0100

    initialDoc - project structure refactor

--- data/processingScripts/dataModels/Deliver.py
+++
@@ -1,6 +0,0 @@
-class Deliver:
-    def __init__(self, purchaseId, purchaseTime, deliveryTime, deliveryCompany):
-        self.purchaseId = purchaseId
-        self.purchaseTime = purchaseTime
-        self.deliveryTime = deliveryTime
-        self.deliveryCompany = deliveryCompany
