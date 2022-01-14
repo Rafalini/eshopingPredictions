72c5b7a2b7b09f202cea0dc84ac0ac8f2c47b7c9
rbosko <rbosko@ittouch.pl>
2022-01-14 12:39:41 +0100

    data merge v4

--- data/data_loader.py
+++ data/data_loader.py
@@ -21,12 +21,10 @@ def splitCategories(products):
             categoryName = "cat_"+str(i)
             products[categoryName] = categoryList[i]
 
-    print(products.head)
-    products = products.drop('category_path', axis=1)
-    print(products.head)
-
     return products
 
+def formatSessions(sessions):
+    return sessions.replace({'event_type': {'BUY_PRODUCT': True, 'VIEW_PRODUCT': False}})
 
 def getDataFromJson(filepath):
     with open(filepath) as f:
@@ -47,12 +45,12 @@ if module_path not in sys.path:
         DATASET_PATH = DATA_PATH+"\\merged_sessions_products_data\\"
 
 if __name__ == "__main__":
-    sessions = getDataFromJson(DATA_PATH+'sessions.jsonl')
+    sessions = formatSessions(getDataFromJson(DATA_PATH+'sessions.jsonl'))
+    products = splitCategories(getDataFromJson(DATA_PATH+'products.jsonl'))
     users = getDataFromJson(DATA_PATH+'users.jsonl')
-    products = getDataFromJson(DATA_PATH+'products.jsonl')
-    products = splitCategories(products)
+
     merged = pd.merge(sessions, users, how="left", on="user_id")
     merged = pd.merge(merged, products, how="left", on="product_id")
-    merged.drop(['street', 'name'], axis=1)
+    merged = merged.drop(['street', 'name', 'category_path'], axis=1)
     # print(merged.head)
     merged.to_csv("merged_sessions_products_data", sep=' ', index=False)
