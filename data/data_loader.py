feba9cfa9a7399797e85ce7516b8ac345f84c223
rbosko <rbosko@ittouch.pl>
2022-01-14 12:30:10 +0100

    data merge v3

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,12 +1,32 @@
-from pathlib import Path
-
 import pandas as pd
-import jsonlines
 import json
 import os
 import sys
 
-import datetime as dt
+
+def splitCategories(products):
+    maxLen = 0
+    lis = ""
+    for index, product in products.iterrows():
+        if maxLen < len(product['category_path'].split(';')):
+           maxLen = len(product['category_path'].split(';'))
+           lis = product
+
+    columns = products.columns
+    for index, product in products.iterrows():
+        categoryList = product[columns.get_loc('category_path')].split(';')
+        for i in range(maxLen - len(categoryList)):
+            categoryList.append("")
+        for i in range(maxLen):
+            categoryName = "cat_"+str(i)
+            products[categoryName] = categoryList[i]
+
+    print(products.head)
+    products = products.drop('category_path', axis=1)
+    print(products.head)
+
+    return products
+
 
 def getDataFromJson(filepath):
     with open(filepath) as f:
@@ -15,6 +35,7 @@ def getDataFromJson(filepath):
     dataFrame = pd.DataFrame(lines)
     dataFrame.columns = ['json_element']
     return pd.json_normalize(dataFrame['json_element'].apply(json.loads))
+
 
 module_path = os.path.abspath(os.path.join('..'))
 if module_path not in sys.path:
@@ -29,6 +50,7 @@ if __name__ == "__main__":
     sessions = getDataFromJson(DATA_PATH+'sessions.jsonl')
     users = getDataFromJson(DATA_PATH+'users.jsonl')
     products = getDataFromJson(DATA_PATH+'products.jsonl')
+    products = splitCategories(products)
     merged = pd.merge(sessions, users, how="left", on="user_id")
     merged = pd.merge(merged, products, how="left", on="product_id")
     merged.drop(['street', 'name'], axis=1)
